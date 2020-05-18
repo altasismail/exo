df8808533a305e8ddd6d11ff32fad07f2fcf7dac
altasismail <altasismail@gmail.com>
2020-05-18 15:13:32 +0200

    first exercice

---
+++ exo1.php
@@ -0,0 +1,23 @@
+
+<!-- Question :
+CREER UN FICHIER poo-exo1.php
+POUR COMPLETER LE CODE...
+(ATTENTION: VOUS NE DEVEZ PAS MODIFIER LE CODE APRES LA LIGNE "CODE NON MODIFIABLE")
+ -->
+ <?php
+// AJOUTER LE CODE POUR AFFICHER coucou
+
+// ... AJOUTER VOTRE CODE DIRECTEMENT ICI ...
+
+class Exo1
+{
+    function say_coucou()
+    {
+        echo "coucou"; 
+    }
+}
+// CODE NON MODIFIABLE 
+// (LAISSER CE CODE TEL QUEL SINON TU PAIES UNE AMENDE...)
+$objet = new Exo1;
+$objet->say_coucou();
+?>
